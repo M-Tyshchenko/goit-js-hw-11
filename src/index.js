20d37db18020ac393f6ddcf6c65187cf8058d705
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-07-10 15:02:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
 import Notiflix from 'notiflix';
 import axios from "axios";
 
-const formEl = document.querySelector('.search-form');
+const apiKey = '13806699-094791483596adb8f8eca5126';
+const baseUrl = 'https://pixabay.com/api';
+axios.defaults.headers.common["x-api-key"] = apiKey;
 
-formEl.addEventListener('submit', (event) => {
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+
+//let queriedImgsArr = [];
+
+formEl.addEventListener('change', (event) => {
+    const inputData = event.target.value;
+    console.log(inputData);
+
+    formEl.addEventListener('submit', (event) => {
     event.preventDefault();
+        fetch(`${baseUrl}/?key=${apiKey}&q=${inputData}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(imgsData => {
+                
+                console.log(imgsData);
+                
+                // imgsData.forEach((image) => {
+                //     const imageCard = `<div class="photo-card">
+                //         <img src="" alt="" loading="lazy" />
+                //         <div class="info">
+                //             <p class="info-item">
+                //             <b>Likes</b>
+                //             </p>
+                //             <p class="info-item">
+                //             <b>Views</b>
+                //             </p>
+                //             <p class="info-item">
+                //             <b>Comments</b>
+                //             </p>
+                //             <p class="info-item">
+                //             <b>Downloads</b>
+                //             </p>
+                //         </div>
+                //         </div>`;
+                //     galleryEl.innerHTML = imageCard;
+                // });
+            })
+            .catch(err => {
+                console.log(err);
+            });
 });
+})
