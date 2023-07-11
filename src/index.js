b9067e6bb892d9cd53a9ed3c149dfb7abfba93d5
M-Tyshchenko <m.tish2406@gmail.com>
2023-07-11 23:22:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const pixabayApi = new PixabayAPI();
 formEl.addEventListener('submit', async (event) => {
     event.preventDefault();
 
+    galleryEl.innerHTML = '';
+    loadMoreBtn.classList.add('is-hidden');
     pixabayApi.q = event.currentTarget.elements['searchQuery'].value;
     
     pixabayApi.page = 1;
@@ -21,20 +23,36 @@ formEl.addEventListener('submit', async (event) => {
 
     try {
         const { data } = await pixabayApi.fetchPhotos();
-        
-        if (!data.hits.length) {
-            galleryEl.innerHTML = '';
-            throw new Error();
-            
+
+        if (!data.hits.length) {           
+            throw new Error();            
         }
-        const imgsData = data.hits;
-        createGallaryCards(imgsData);
+
+        createGallaryCards(data.hits);
         loadMoreBtn.classList.remove('is-hidden');
     }
     catch (err) {
         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     }
 });
+
+loadMoreBtn.addEventListener('click', async () => {
+    pixabayApi.page += 1;
+
+    try {
+        const { data } = await pixabayApi.fetchPhotos();
+        const totalPages = Math.ceil(data.totalHits / pixabayApi.perPage);
+        
+        if (pixabayApi.page === totalPages) {
+            loadMoreBtn.classList.add('is-hidden');
+            Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+        }
+        createGallaryCards(data.hits);
+
+    } catch (error) {
+        console.log(error.message);
+    }
+})
   
 function createGallaryCards(images) {
     for (const image of images) {
